b18db9d6aa3cf560a457a3200fcbb19bfec53e20
Bella Becker <94186726+Huck21@users.noreply.github.com>
2021-12-18 17:54:29 +0000

    I edited the code in part 1 and wrote code for parts 2, 3, and 4

--- part1.py
+++ part1.py
@@ -4,14 +4,14 @@ PART 1
 ******
 The program below is supposed to ask the user to enter an integer input, and print the product of all of the positive integers less than or equal to that number (also known as the factorial of the number). For example, if the user enter 5, the program should print 120 (since 5! = 5*4*3*2*1 = 120).
 
-The program doesn't work quite right yet, though. Fix the program so that it runs the way it should (there should be at 3 things you need to change.)
+The program doesn't work quite right yet, though. Fix the program so that it runs the way it should (there should be at least 3 things you need to change.)
 """
 
 number = int(input("What factorial are you looking for? "))
 
-product = 0
+product = 1
 
-for i in range(number):
+for i in range(1, number + 1): #not inclusive
   product = product * i
 
 print(product)
